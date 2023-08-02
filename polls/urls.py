87c970820302b89e92242d7432158c9aac880fa7
Rafael Ribeiro <klkp.398@gmail.com>
2023-08-02 09:08:17 -0300

    Alterado perguntas_recentes

--- polls/urls.py
+++ polls/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns =  [
     path('', views.index, name="index"),
     path("sobre/", views.sobre, name="sobre"), 
-    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas')
+    path("perguntas/", views.ultimas_perguntas, name='ultimas_perguntas')
 ]
