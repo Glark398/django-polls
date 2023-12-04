bb7ca73eec2513e2a61cb529804a41e934244a7d
Rafael Ribeiro <107559031+Glark398@users.noreply.github.com>
2023-12-04 07:56:53 -0300

    Enquetes Votadas pelo Usuário 2

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
             views.AccountUpdateView.as_view(),
             name="account_edit"
         ),
+    path('account/me',
+            views.AccountTemplateView.as_view(),
+            name="account_detail" 
+    ),
 ]
